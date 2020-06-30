d990b5d29b74af0a58b993bd590dd77b6b593e08
Silviu Marinescu <f.silviumarinescu@gmail.com>
2020-07-01 02:11:23 +0300

    wip

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,73 @@
+<script>
+  console.log("aaaa");
+//   fetch("https://random-number-generator1.p.rapidapi.com/", {
+//     method: "POST",
+//     headers: {
+//       "x-rapidapi-host": "random-number-generator1.p.rapidapi.com",
+//       "x-rapidapi-key": "7f67c32095mshe15fe37f0ed1934p1d570djsnafbee9a08226",
+//       "content-type": "application/x-www-form-urlencoded"
+//     },
+//     body: {
+//       num: "10"
+//     }
+//   })
+//     .then(response => {
+//       console.log(response);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  figure {
+    margin: 0 0 1em 0;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  p {
+    margin: 1em auto;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Sapper project template</title>
 </svelte:head>
 
 <h1>Great success!</h1>
 
 <figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
+  <img alt="Success Kid" src="successkid.jpg" />
+  <figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>
+  <strong>
+    Try editing this file (src/routes/index.svelte) to tessdft live reloading.
+  </strong>
+</p>
