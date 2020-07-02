14491979bb4f75e72e35529afc2e33611eda3f09
Silviu Marinescu <f.silviumarinescu@gmail.com>
2020-07-02 14:51:05 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", async (req, res) => {
+  res.set("Cache-Control", "s-maxage=10, stale-while-revalidate");
+  const content = new Promise((accept, reject) => {
+    setTimeout(() => {
+      accept(Math.random());
+    }, 5000);
+  });
+  res.send("Hello World!" + (await content));
+});
 
 module.exports = app;
