15d1b4f53c0752afe00d04bfe66bb4c46f607114
Silviu Marinescu <f.silviumarinescu@gmail.com>
2020-07-02 18:03:35 +0300

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,52 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const { basename } = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
+  entry: "./src/index.js",
+  devServer: {
+    historyApiFallback: true,
+  },
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
-      }
-    ]
+        loader: "vue-loader",
+      },
+    ],
   },
   plugins: [
-    // make sure to include the plugin!
-    new VueLoaderPlugin()
-  ]
-}
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new HtmlWebpackPlugin({
+      title: "ssr",
+      meta: {
+        viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+      },
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./static/favicon.svg",
+      mode: "webapp",
+      devMode: "webapp",
+      favicons: {
+        appName: "Event Tests",
+        appDescription: "Event Tests",
+        developerName: "Converdy",
+        developerURL: null,
+        background: "#d8d8d8",
+        theme_color: "#08bfb0",
+        icons: {
+          coast: false,
+          yandex: false,
+        },
+      },
+      inject: (htmlPlugin) =>
+        basename(htmlPlugin.options.filename) === "index.html",
+    }),
+    new VueLoaderPlugin(),
+  ],
+};
