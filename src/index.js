bea2f965733b485ae6289f65519826df8e8348ba
Silviu Marinescu <f.silviumarinescu@gmail.com>
2020-07-02 15:29:40 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -2,26 +2,27 @@ const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
-  res.set("Cache-Control", "s-maxage=10, stale-while-revalidate");
+  try {
+    const renderer = require("vue-server-renderer").createRenderer({
+      template: require("fs").readFileSync("../static/index.html", "utf-8"),
+    });
+    const app = require("../static/vue.js");
 
-  const renderer = require("vue-server-renderer").createRenderer({
-    template: require("fs").readFileSync("./index.html", "utf-8"),
-  });
+    const data = {
+      title: "Vue SSR Tutorial aaaaaaaa" + Math.random(),
+    };
 
-  const data = {
-    title: "Vue SSR Tutorial aaaaaaaa" + Math.random(),
-  };
-
-  const app = require("./vue.js");
-
-  renderer.renderToString(app, data, (err, html) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Internal Server Error");
-      return;
-    }
-    res.send(html);
-  });
+    renderer.renderToString(app, data, (err, html) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+      res.send(html);
+    });
+  } catch (err) {
+    res.send(__dirname + 'aaaaaaaaa');
+  }
 });
 
 module.exports = app;
